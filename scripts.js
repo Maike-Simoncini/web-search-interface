402cfd9928991d6c1c51fd559a07f6894836e059
Maike Simoncini da Silva <maikesimoncinims@gmail.com>
2025-02-17 14:06:30 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function search() {
+    const query = document.getElementById('searchInput').value;
+    if (query) {
+        const url = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
+        window.open(url, '_blank');
+    } else {
+        alert('Por favor, digite algo para pesquisar.');
+    }
+}
+
+// Permitir pesquisa ao pressionar "Enter"
+document.getElementById('searchInput').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        search();
+    }
+});
